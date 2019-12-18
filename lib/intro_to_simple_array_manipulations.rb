0df82573ebf9b9bf89aeead94156078dead2f62d
Amy Franz <amy.franz@gmail.com>
2019-12-18 21:44:04 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -11,8 +11,7 @@ def using_pop(array)
 end
 
 def pop_with_args(array)
-  array.pop 
-  array.pop 
+  array.pop(2) 
 end
 
 def using_shift(array)
@@ -20,5 +19,8 @@ def using_shift(array)
 end
 
 def shift_with_args(array)
-  array.shift 
+  array.shift(2)
 end
+
+def using_concat(array1, array2)
+  array1.concat
